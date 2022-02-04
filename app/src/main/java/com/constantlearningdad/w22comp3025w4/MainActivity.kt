0d25a89d1112fa84905ad38346de6a69b3ce3ed3
JaretWright <jaret.wright@georgiancollege.ca>
2022-02-04 09:58:12 -0500

    Added the ability to change from the MainActivity to the PassDetailsActivity and we used a StringExtra in the intent to pass the name over.

--- app/src/main/java/com/constantlearningdad/w22comp3025w4/MainActivity.kt
+++ app/src/main/java/com/constantlearningdad/w22comp3025w4/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.constantlearningdad.w22comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.constantlearningdad.w22comp3025w4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //configure the view binding to connect view objects with the controller class
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //connect binding variable with the actual ActivityMainBinding class
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+       //configure the content view to use the binding class instead of the activity_main.xml
+        setContentView(binding.root)
+
+        binding.passDetailsButton.setOnClickListener {
+            var name = binding.nameEditText.text.toString()
+
+            if (name.isNotEmpty())
+            {
+                //pass the information to the PassDetailsActivity
+                //create an explicit intent to navigate to the PassDetailsActivity
+                var intent = Intent(this, PassDetailsActivity::class.java)
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }
+            else
+            {
+                Toast.makeText(this,"Please enter your name", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
